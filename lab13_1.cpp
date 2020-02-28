73a2186a287eebf65f2200fbe4b047cf1ca442c3
aomsin1822544 <pspsas333_111@hotmail.com>
2020-02-28 15:06:18 +0700

    1.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -9,54 +9,35 @@ void swap(T d[],int x,int y){
     d[y] = temp;
 }
 
-
-
-//Write your code here.
- 
-   
-
-
-    
-
-	template <typename T>
+template <typename T>
 void insertionSort(T d[],int N){
-int s=0;
-	for(int i = 1 ; i<N;i++){
-		for (int j = 0;j < N;j++){
-			if(j==i){
-				cout<< "\b[" << d[j] << "]" << " ";
+	int s=0;
+	for (int i = 1; i < N; i++){
+		for (int j = 0; j < N; j++){
+			if ( j == i){
+				cout << "\b[" << d[j] << "]" << " ";
 				s=d[j];
-			}
-			else{
+			}else{
 				cout << d[j] << " ";
 			}
-			
 		}
-	cout << "=>";
-	for(int j=0;j<N;j++){
-		if(d[j]==s && j<i+1 && d[j+1] != s){
-			cout << "\b["<< d[j] << "]" << " ";
+		cout << "=> ";
+		for (int k = i; k > 0 ; k--){
+			if ( d[k] > d[k-1]){
+				swap(d,k,k-1);
+				
+			}
 		}
-		else{
-			cout << d[j] << " ";
+			for (int j = 0; j < N; j++){
+			if ( d[j] == s && j<i+1 && d[j+1] != s){
+				cout << "\b[" << d[j] << "]" << " ";
+			}else{
+				cout << d[j] << " " ;
+			}
 		}
-		
-	}
-	
-	
-	cout << endl;
-	for(int j=i;j>0;j--){
-		if (d[j]>d[j-1]){
-			swap(d, j , j-1);
-		}
-    
-}
+		cout << "\n";
 	}
 }
-         
-
-
- 
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -65,7 +46,7 @@ int main(){
 	cout << "\n\n";
 	
 	insertionSort(a,10);
-				
+	
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
